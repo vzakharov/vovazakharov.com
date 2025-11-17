a88f56b2515e3fdeba033ed9c345926f31ed5aa4
Vova Zakharov <vzakharov@gmail.com>
2025-11-17 17:02:47 +0300

    Revert "Update basePath and enable trailingSlash in Next.js config"
    This reverts commit 67a54956f083e8cd13f60df99f90c8b9f1790140.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/vovazakharov.com',
-  trailingSlash: true,
+  basePath: '',
   images: {
     unoptimized: true,
   },
